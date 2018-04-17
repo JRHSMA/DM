32f16238c2133289f5cc1d7faeb8cfec0747a5d2
RBHSMA <RBHSMA@users.noreply.github.com>
2018-04-17 16:11:58 +0200

    more

--- DM/src/Main.java
+++ DM/src/Main.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
 
 public class Main {
+	public static void main(String args[]) {
+		System.out.println("Please Select The Aufgabe You Want To See\n1.\n2.\n3.");
+		Scanner sc = new Scanner(System.in);
+		int auf;
+		boolean inRight = false;
+		while (inRight == false) {
+			auf = sc.nextInt();
+			if (auf < 1 || auf > 3) {
+				System.out.println("FALSCH!!!\nNOCHMAL");
+			} else {
+				inRight = true;
+			}
+		}
+	}
 
 }
