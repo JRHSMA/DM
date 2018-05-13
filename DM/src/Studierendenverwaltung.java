cacb8a53660bd6f6dc91db8a28022b87e3ae61a2
RBHSMA <1710317@stud.hs-mannheim.de>
2018-05-13 21:28:52 +0200

    stuff

--- DM/src/Studierendenverwaltung.java
+++ DM/src/Studierendenverwaltung.java
@@ -47,9 +47,6 @@ public class Studierendenverwaltung {
 		
 	}
 
-	public static void main(String[] args) {
-		new Studierendenverwaltung();	
-	}
 
 	public void DatenAusDbEinlesen() {
 
@@ -509,7 +506,7 @@ public class Studierendenverwaltung {
 	}
 
 	//person
-	public void personHinzufuegen(int id, String vorname, String nachname, String geburtsdatum, boolean maennlich) {
+	public void personHinzufuegen(String vorname, String nachname, String geburtsdatum, boolean maennlich) {
 		// Person hinzufügen DB
 		DB datenzugriff = null;
 		boolean dbEinfuegen = false;
