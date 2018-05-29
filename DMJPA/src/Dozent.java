5e89d28a6d6da9eb46ef1979607826b761b12fb4
JRHSMA <1712189@stud.hs-mannheim.de>
2018-05-29 19:54:15 +0200

    jpa test

---
+++ DMJPA/src/Dozent.java
@@ -0,0 +1,72 @@
+
+public class Dozent {
+
+	private int personalNr;
+	private String kuerzel;
+	private Person person;
+	private Fakultaet fakultaet;
+
+	public Dozent(int personalNr, String kuerzel, Fakultaet fakultaet, Person person) {
+		setPersonalNr(personalNr);
+		setKuerzel(kuerzel);
+		setFakultaet(fakultaet);
+		setPerson(person);
+	}
+
+	public boolean aendern(String kuerzel, Fakultaet fakultaet, Person person) {
+		try {
+			setKuerzel(kuerzel);
+			setFakultaet(fakultaet);
+			setPerson(person);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	private void setPersonalNr(int personalNr) {
+		this.personalNr = personalNr;
+	}
+
+	public int getPersonalNr() {
+		return personalNr;
+	}
+
+	private void setKuerzel(String kuerzel) {
+		this.kuerzel = kuerzel;
+	}
+
+	public String getKuerzel() {
+		return kuerzel;
+	}
+
+	public Person getPerson() {
+		return person;
+	}
+
+	public int getPersonID() {
+		return person.getId();
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+
+	public Fakultaet getFakultaet() {
+		return fakultaet;
+	}
+
+	public String getFakultaetName() {
+		return fakultaet.getName();
+	}
+
+	public void setFakultaet(Fakultaet fakultaet) {
+		this.fakultaet = fakultaet;
+	}
+
+	@Override
+	public String toString() {
+		return "PersonalNr: " + personalNr + ", Kürzel: " + kuerzel + ", FakultätID: " + fakultaet.getId() + ", PersonID: "
+				+ person.getId();
+	}
+}
---
+++ DMJPA/src/Erhaelt.java
@@ -0,0 +1,32 @@
+
+public class Erhaelt {
+
+	private Dozent dozent;
+	private Stundenplan stundenplan;
+	
+	public Erhaelt(Dozent dozent, Stundenplan stundenplan){
+		setDozent(dozent);
+		setStundenplan(stundenplan);
+	}
+
+	public Dozent getDozent() {
+		return dozent;
+	}
+
+	public void setDozent(Dozent dozent) {
+		this.dozent = dozent;
+	}
+
+	public Stundenplan getStundenplan() {
+		return stundenplan;
+	}
+
+	public void setStundenplan(Stundenplan stundenplan) {
+		this.stundenplan = stundenplan;
+	}
+	
+	@Override
+	public String toString() {
+		return "PersonalNr: "+ dozent.getPersonalNr() + ", StundenplanID: "+ stundenplan.getId();
+	}
+}
